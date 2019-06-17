398108892fe55cf51023e45e41dd9ed1ed03fa74
Tomasz Figas <tomasz.figas@gmail.com>
2019-06-17 23:30:07 +0200

    AHand class init comp setup

--- Plugins/OculusDevelopment/Source/OculusDevelopment/Private/Hand.cpp
+++ Plugins/OculusDevelopment/Source/OculusDevelopment/Private/Hand.cpp
@@ -2,12 +2,47 @@
 
 
 #include "Hand.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SplineMeshComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 
 // Sets default values
 AHand::AHand()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	VRRoot = CreateDefaultSubobject<USceneComponent>(TEXT("VRRoot"));
+	RootComponent = VRRoot;
+
+	HandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("HandSkeletalMesh"));
+	HandMesh->SetupAttachment(RootComponent);
+
+	GrabRoot = CreateDefaultSubobject<USceneComponent>(TEXT("GrabSceneRoot"));
+	GrabRoot->SetupAttachment(HandMesh);
+
+	GrabTrigger = CreateDefaultSubobject<UCapsuleComponent>(TEXT("GrabTriggerCollisionCapsule"));
+	GrabTrigger->SetupAttachment(HandMesh);
+
+	LaserMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LaserPointerMesh"));
+	LaserMesh->SetupAttachment(RootComponent);
+
+	TeleportMarker = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TeleportMarkerMesh"));
+	TeleportMarker->SetupAttachment(RootComponent);
+
+	TeleportSplineMesh = CreateDefaultSubobject<USplineMeshComponent>(TEXT("TeleportSplineMesh"));
+	TeleportSplineMesh->SetupAttachment(RootComponent);
+
+	PhysicsHandle = CreateDefaultSubobject<UPhysicsHandleComponent>(TEXT("PhysicsHandle"));
+
+	// Set default value for finger axis vars
+	ThumbFingerAxis = 0.8f;
+	IndexFingerAxis = 0.8f;
+	MiddleFingerAxis = 0.8f;
+	RingFingerAxis = 0.8f;
+	PinkyFingerAxis = 0.8f;
 
 }
 
--- Plugins/OculusDevelopment/Source/OculusDevelopment/Public/Hand.h
+++ Plugins/OculusDevelopment/Source/OculusDevelopment/Public/Hand.h
@@ -6,15 +6,37 @@
 #include "GameFramework/Actor.h"
 #include "Hand.generated.h"
 
-UCLASS()
+UCLASS(config=Game, Blueprintable)
 class OCULUSDEVELOPMENT_API AHand : public AActor
 {
 	GENERATED_BODY()
 
-	
-public:	
-	// Sets default values for this actor's properties
-	AHand();
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class USceneComponent* VRRoot;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class USkeletalMeshComponent* HandMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class USceneComponent* GrabRoot;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class UCapsuleComponent* GrabTrigger;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* LaserMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* TeleportMarker;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class USplineMeshComponent* TeleportSplineMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true"))
+		class UPhysicsHandleComponent* PhysicsHandle;
 
 protected:
 	// Called when the game starts or when spawned
@@ -23,6 +45,16 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	// Sets default values for this actor's properties
+	AHand();
+
+	// Hand anim finger variables
+	float ThumbFingerAxis;
+	float IndexFingerAxis;
+	float MiddleFingerAxis;
+	float RingFingerAxis;
+	float PinkyFingerAxis;
 
 	
 
